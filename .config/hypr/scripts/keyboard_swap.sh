1e50bd7045aef9e1b6c699a8ac7c2ede629e7b02
Craacky <cbxj.ysa@gmail.com>
2025-10-15 15:21:36 +0300

    update dunst script for kbd layout

--- .config/hypr/scripts/keyboard_swap.sh
+++ .config/hypr/scripts/keyboard_swap.sh
@@ -5,11 +5,13 @@
 # │                    hyprland script                          │
 # ╰─────────────────────────────────────────────────────────────╯
 
-# ── Get script directory ──
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
 # ── Get the main keyboard layout ──
 LAYOUT_MAIN=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '\"' '{if ($2=="active_keymap") print $4}')
 
-# ── Send notification with current layout ──
-notify-send -a "Keyboard Layout" -r 91190 -t 800 -i "${SCRIPT_DIR}/../dunst/icons/keyboard.svg" "${LAYOUT_MAIN}"
+ICON_SRC="$HOME/.config/hypr/icons/keyboard.svg"
+ICON_TMP="/tmp/keyboard_icon.svg"
+
+sed "s/fill=\"#.*\"/fill=\"#a6e3a1\"/g" "$ICON_SRC" > "$ICON_TMP"
+
+notify-send -a "Keyboard Layout" -r 91190 -t 800 -i "$ICON_TMP" "${LAYOUT_MAIN}"
+
